efd8fba13eacaf0194394e30dc858f718b7041f9
bidtime <bidtime@126.com>
2017-12-08 12:01:40 +0800

    rm utils

--- table2xml.dpr
+++ table2xml.dpr
@@ -3,11 +3,11 @@ program table2xml;
 uses
   Forms,
   uFrmMain in 'src\uFrmMain.pas' {frmMain},
-  uCharSplit in 'src\utils\uCharSplit.pas',
-  uCharUtils in 'src\utils\uCharUtils.pas',
   uFrmSetting in 'src\frm\uFrmSetting.pas' {frmSetting},
   uColumnPro in 'src\utils\uColumnPro.pas',
-  uTablePro in 'src\utils\uTablePro.pas';
+  uTablePro in 'src\utils\uTablePro.pas',
+  uCharSplit in '..\delphiutils\src\utils\uCharSplit.pas',
+  uCharUtils in '..\delphiutils\src\utils\uCharUtils.pas';
 
 {$R *.res}
 
